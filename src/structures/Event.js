93afc600c765f53069620fc1405349046d9bf9e7
Frangu Vlad <contact@kingdgrizzle.ml>
2018-02-27 22:28:32 +0200

    MUH DOCS

--- src/structures/Event.js
+++ src/structures/Event.js
@@ -1,13 +1,71 @@
+/**
+ * An event that will be processed by the {@link Delphinium} instance.
+ */
 class Event {
+	/**
+	 * Options that are passed when creating a new event
+	 * @typedef {object} EventOptions
+	 * @prop {string} [name] The event name
+	 * @prop {boolean} [enabled=null] If the event should be enabled
+	 * @prop {boolean} [gateway=null] If this event is a gateway event
+	 * @prop {boolean} [lava=null] If this event is a LavaPlayer event
+	 * @prop {boolean} [rpc=null] If this event is an RPc event
+	 */
+
+	/**
+	 * Creates an instance of Event.
+	 * @param {Delphinium} client The client
+	 * @param {EventOptions} [options={}] The event options
+	 * @memberof Event
+	 */
 	constructor(client, options = {}) {
+		/**
+		 * The client instance
+		 * @type {Delphinium}
+		 */
 		this.client = client;
+
+		/**
+		 * The event name
+		 * @type {string}
+		 */
 		this.name = options.name;
+
+		/**
+		 * If this event is enabled
+		 * @type {boolean}
+		 * @default false
+		 */
 		this.enabled = Boolean(options.enabled);
+
+		/**
+		 * If this event is a gateway event
+		 * @type {boolean}
+		 * @default false
+		 */
 		this.gateway = Boolean(options.gateway);
+
+		/**
+		 * If this event is a LavaPlayer event
+		 * @type {boolean}
+		 * @default false
+		 */
 		this.lava = Boolean(options.lava);
+
+		/**
+		 * If this event is a RPC event
+		 * @type {boolean}
+		 * @default false
+		 */
 		this.rpc = Boolean(options.rpc);
 	}
 
+	/**
+	 * Runs the event if it is enabled.
+	 * @param {Array<*>} args The arguments received
+	 * @private
+	 * @memberof Event
+	 */
 	async _run(...args) {
 		if (this.enabled) {
 			try {
@@ -18,6 +76,12 @@ class Event {
 		}
 	}
 
+	/**
+	 * Function that takes the event input and processes it.
+	 * @param {Array<*>} args The raw arguments received
+	 * @abstract
+	 * @memberof Event
+	 */
 	run(...args) {} // eslint-disable-line no-unused-vars
 }
 
