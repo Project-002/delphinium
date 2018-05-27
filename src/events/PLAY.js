b03a2543eaf56b61e0408d6c0855c5ca6f0bed5e
Crawl <icrawltogo@gmail.com>
2018-05-27 15:33:59 +0200

    fix: accept playlists

--- src/events/PLAY.js
+++ src/events/PLAY.js
@@ -7,7 +7,7 @@ class PLAY extends Event {
 
 	async run(packet, { ack }) {
 		const queue = this.client.lavalink.queues.get(packet.guild);
-		await queue.add([packet.track]);
+		await queue.add(...packet.tracks.map(p => p.track));
 		if (!queue.player.playing && !queue.player.paused) await queue.start();
 		ack();
 	}
